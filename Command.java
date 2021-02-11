6da481c77e2c9f01a517b6f4c564114fd80e3c20
LaTristan <50165836+LaTristan@users.noreply.github.com>
2021-02-11 19:14:14 +0800

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   Command.java 	modified:   Noodle.java

--- Command.java
+++ Command.java
@@ -1,9 +1,12 @@
 public class Command {
     public static void main(String[] args){
-        Noodle noodle=new Noodle("Hot Dry Noodles");
+        Noodle noodle=new Noodle("Hot Dry Noodles",10);
+        Noodle noodle2=new Noodle("ZJM",8);
         BuyNoodle buyNoodle=new BuyNoodle(noodle);
+        BuyNoodle buyNoodle2=new BuyNoodle(noodle2);
         Broker broker=new Broker();
         broker.takeOrder(buyNoodle);
+        broker.takeOrder(buyNoodle2);
         broker.placeOrders();
     }
 }
--- Noodle.java
+++ Noodle.java
@@ -1,8 +1,9 @@
 public class Noodle {
     private String name;
-    private int quantity=10;
-    public Noodle(String name){
+    private int quantity;
+    public Noodle(String name,int q){
         this.name=name;
+        quantity=q;
     }
     public void buy(){
         System.out.println("Noodle "+this.name+" quantity "+quantity+" bought");
